0d3163a23dc287cbb79c1f7b7ac77d812a6241a8
Ganesh Raj Manandhar <ganesh.manandhar@ekbana.info>
2023-07-25 18:08:14 +0545

    feat: google login

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,24 @@
-
-
+import { useSession, signIn, signOut } from "next-auth/react";
 export default function Home() {
+  const { data: session } = useSession();
+  if (session) {
+    return (
+      <>
+        Signed in as {session?.user?.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    );
+  }
   return (
-    <div className={'bg-red-500'}>text</div>
-  )
+    <div className="bg-blue-600 w-screen h-screen flex items-center">
+      <div className="text-center w-full">
+        <button
+          className="bg-white p-2 px-4 rounded-lg"
+          onClick={() => signIn("google")}
+        >
+          Login with Google
+        </button>
+      </div>
+    </div>
+  );
 }
