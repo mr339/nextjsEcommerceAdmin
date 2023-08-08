ad94966fbb4e1733352a9828234cfff9afced619
Ganesh Raj Manandhar <ganesh.manandhar@ekbana.info>
2023-08-08 19:48:45 +0545

    feat: making admin panel mobile responsive

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,28 +1,54 @@
 import { useSession, signIn } from "next-auth/react";
 import Nav from "@/components/Nav";
+import { useState } from "react";
+import Logo from "@/components/Logo";
 
 export default function Layout({ children }: any) {
   const { data: session } = useSession();
-  if (session) {
+  const [showNav, setShowNav] = useState(false);
+  if (!session) {
     return (
-      <div className="bg-bgGray min-h-screen flex">
-        <Nav />
-        <div className="bg-white flex-grow mt-2 mr-2 mb-2 rounded-lg p-4">
-          {children}
+      <div className="bg-bgGray w-screen h-screen flex items-center">
+        <div className="text-center w-full">
+          <button
+            className="bg-white p-2 px-4 rounded-lg"
+            onClick={() => signIn("google")}
+          >
+            Login with Google
+          </button>
         </div>
       </div>
     );
   }
 
   return (
-    <div className="bg-bgGray w-screen h-screen flex items-center">
-      <div className="text-center w-full">
-        <button
-          className="bg-white p-2 px-4 rounded-lg"
-          onClick={() => signIn("google")}
-        >
-          Login with Google
+    <div className="bg-bgGray min-h-screen">
+      <div className="block md:hidden flex items-center p-4">
+        <button onClick={() => setShowNav(true)}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
         </button>
+        <div className="flex grow justify-center">
+        <Logo />
+        </div>
+      </div>
+      <div className="flex">
+        <Nav show={showNav} />
+        <div className="bg-white flex-grow mt-2 mr-2 mb-2 rounded-lg p-4">
+          {children}
+        </div>
       </div>
     </div>
   );
