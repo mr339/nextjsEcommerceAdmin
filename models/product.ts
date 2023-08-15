560f8bda25d1391518c88ee7dcb645c48c2da482
Ganesh Raj Manandhar <ganesh.manandhar@ekbana.info>
2023-08-15 10:02:35 +0545

    chore: added updateddate on model

--- models/product.ts
+++ models/product.ts
@@ -1,12 +1,17 @@
 import mongoose, { Schema, model, models } from "mongoose";
 
-const ProductSchema = new Schema({
-  title: { type: String, required: true },
-  description: String,
-  images: [{ type: String }],
-  price: { type: Number, required: true },
-  category: { type: mongoose.Types.ObjectId, ref: "Category" },
-  properties: { type: Object },
-});
+const ProductSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    description: String,
+    images: [{ type: String }],
+    price: { type: Number, required: true },
+    category: { type: mongoose.Types.ObjectId, ref: "Category" },
+    properties: { type: Object },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 export const Product = models.Product || model("Product", ProductSchema);
